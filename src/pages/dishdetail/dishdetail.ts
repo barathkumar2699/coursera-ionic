8e60947b4d03d26b9fb07345c7f0a71b5c2bd837
Barath <barath6.99@gmail.com>
2019-11-25 22:12:45 +0530

    Alerts, Toasts and Loading

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
@@ -7,10 +7,9 @@ import { FavoriteProvider } from '../../providers/favorite/favorite';
 /**
  * Generated class for the DishdetailPage page.
  *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
   selector: 'page-dishdetail',
@@ -25,22 +24,27 @@ export class DishdetailPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     @Inject('BaseURL') private BaseURL,
-    private favoriteservice: FavoriteProvider) {
+    private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController) {
     this.dish = navParams.get('dish');
+    this.favorite = favoriteservice.isFavorite(this.dish.id);
     this.numcomments = this.dish.comments.length;
     let total = 0;
     this.dish.comments.forEach(comment => total += comment.rating );
     this.avgstars = (total/this.numcomments).toFixed(2);
-    this.favorite = favoriteservice.isFavorite(this.dish.id)
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DishdetailPage');
   }
 
   addToFavorites() {
     console.log('Adding to Favorites', this.dish.id);
     this.favorite = this.favoriteservice.addFavorite(this.dish.id);
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DishdetailPage');
+    this.toastCtrl.create({
+      message: 'Dish ' + this.dish.id + ' added as favorite successfully',
+      position: 'middle',
+      duration: 3000}).present();
   }
 
 }
