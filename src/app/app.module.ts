7df28e3852d361d5835c0cea8eeb88602fbef11e
Barath <barath6.99@gmail.com>
2019-11-26 19:23:39 +0530

    Notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
 import { HttpModule } from '@angular/http';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
@@ -13,18 +12,16 @@ import { ContactPage } from '../pages/contact/contact';
 import { DishdetailPage } from '../pages/dishdetail/dishdetail';
 import { FavoritesPage } from '../pages/favorites/favorites';
 import { ReservationPage } from '../pages/reservation/reservation';
-import {CommentPage } from '../pages/comment/comment';
+import { CommentPage } from '../pages/comment/comment';
 import { LoginPage } from '../pages/login/login';
-
-
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
 import { PromotionProvider } from '../providers/promotion/promotion';
 import { ProcessHttpmsgProvider } from '../providers/process-httpmsg/process-httpmsg';
-
 
 import { baseURL } from '../shared/baseurl';
 import { FavoriteProvider } from '../providers/favorite/favorite';
@@ -44,15 +41,15 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
     HttpModule,
+    IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    AboutPage, 
+    AboutPage,
     MenuPage,
     ContactPage,
     DishdetailPage,
@@ -69,9 +66,9 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
     LeaderProvider,
     PromotionProvider,
     ProcessHttpmsgProvider,
-    {provide: 'BaseURL', useValue: baseURL},
+    { provide: 'BaseURL', useValue: baseURL },
     FavoriteProvider,
-    
+    LocalNotifications
   ]
 })
 export class AppModule {}
--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -4,6 +4,7 @@ import { Dish } from '../../shared/dish';
 import { Observable } from 'rxjs/Observable';
 import { DishProvider } from '../dish/dish';
 import { Storage } from '@ionic/storage';
+import { LocalNotifications, LocalNotificationsOriginal } from '@ionic-native/local-notifications';
 
 /*
   Generated class for the FavoriteProvider provider.
@@ -16,7 +17,8 @@ export class FavoriteProvider {
   favorites: Array<any>;
   
   constructor(public http: Http, private dishservice: DishProvider,
-    private storage: Storage) {
+    private storage: Storage,
+    private localNotifications: LocalNotificationsOriginal) {
     console.log('Hello FavoriteProvider Provider');
     storage.get('favorites').then(favorites => {
       if (favorites) {
@@ -33,6 +35,12 @@ export class FavoriteProvider {
     if (!this.isFavorite(id)) {
       this.favorites.push(id);
       this.storage.set('favorites', this.favorites);
+
+      // Schedule a single notification
+      this.localNotifications.schedule({
+        id: id,
+        text: 'Dish ' + id + 'added as a favorite successfully'
+      });
     }      
     console.log('favorites', this.favorites);
     return true;
